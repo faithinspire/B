e0ad94c27cd59367c0c7618e987b407cfbffcaa0
Braidly Dev <dev@braidly.com>
2026-03-15 05:32:20 -0700

    Fix: Remove unused useCallback import from braider messages page

--- app/(braider)/braider/messages/[booking_id]/page.tsx
+++ app/(braider)/braider/messages/[booking_id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState, useRef, useCallback } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useRouter, useParams } from 'next/navigation';
 import { useSupabaseAuthStore } from '@/store/supabaseAuthStore';
 import { BraiderPageLayout } from '@/app/components/BraiderPageLayout';
